f7e8bda75312e9cdc34a85758b8867c6631ae774
Nurul Islam <devnurul11@gmail.com>
2023-07-14 02:16:14 +0600

    success jwt token

---
+++ Inventory/app/Helper/JWTToken.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Helper;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTToken {
+    public static function createJWTToken($userEmail): string {
+        $key = env('JWT_KEY');
+
+        $payload = [
+            'iss' => "lumen-jwt",    // issuer of the token
+            //'sub' => $user->id,      // subject of the token
+            'iat' => time(),         // time when the JWT was issued
+            'exp' => time() + 60 * 60,  // time when the JWT will expire
+            'userEmail' => $userEmail
+        ];
+
+        return JWT::encode($payload, $key, 'HS256');
+    }
+
+    public static function DecodeToken($token) {
+        try {
+            $key = env('JWT_KEY');
+            $decoded = JWT::decode($token, new Key($key, 'HS256'));
+
+            return $decoded->userEmail;
+        } 
+        catch (Exception $e) {
+            return "Unauthorized";
+        }
+    }
+}
--- Inventory/app/Http/Controllers/UserController.php
+++ Inventory/app/Http/Controllers/UserController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Helper\JWTToken;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    function userLogin(){
-    
+    function userLogin(Request $request){
+            $res = User::where($request->input())->count();
+
+            if ($res == 1){
+                //token create
+                $token = JWTToken::createJWTToken($request->input('email'));
+                return response()->json([
+                'msg' =>'Succes',
+                'data' => $token
+                ]);
+            }
+        else {
+            return response()->json([
+                'msg' =>'Faild',
+                'data' => 'unthurized'
+            ]);
+        }
     }
-    function userRegister(Request $request){
-        return User::create($request->input());
-    }
+        function userRegister(Request $request){
+            return User::create($request->input());
+        }
     function OTPToMail(){
     
     }
@@ -27,4 +43,5 @@ class UserController extends Controller
     
     }
 
+    
 }
