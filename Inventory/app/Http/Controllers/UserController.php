a7d0054e5466ffb7c367bbbf67c0e646080706b4
Nurul Islam <devnurul11@gmail.com>
2023-07-14 12:44:26 +0600

    add profile done

--- Inventory/app/Http/Controllers/UserController.php
+++ Inventory/app/Http/Controllers/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Mail\OTPMail;
 use App\Helper\JWTToken;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Console\Input\Input;
 
 class UserController extends Controller
 {
@@ -15,10 +18,7 @@ class UserController extends Controller
             if ($res == 1){
                 //token create
                 $token = JWTToken::createJWTToken($request->input('email'));
-                return response()->json([
-                'msg' =>'Succes',
-                'data' => $token
-                ]);
+                return response()->json(['msg' =>'Succes','data' => $token]);
             }
         else {
             return response()->json([
@@ -30,14 +30,42 @@ class UserController extends Controller
         function userRegister(Request $request){
             return User::create($request->input());
         }
-    function OTPToMail(){
-    
+    function OTPToMail(Request $request){
+        $UserMail = $request->input('email');
+        $otp = rand(1000, 9999);
+
+        //mail chaeck method
+        $res = User::where($request->input())->count();
+        if ($res == 1) {
+
+            //mail sent method
+            Mail::to($UserMail)->send(new OTPMail($otp));
+
+            //database update by otp
+            User::where($request->input())->update(['otp'=>$otp]);
+            return response()->json(['msg' =>'Succes', 'data'=>'Otp sent to you mail']);
+        }else{
+
+            return response()->json([
+                'msg' =>'Faild',
+                'data' => 'unthurized'
+            ]);
+        }
     }
-    function OTPVarified(){
-    
+    function OTPVarified(Request $request){
+        $res = User::where($request->input())->count();
+        if($res ==1 ){
+        User::where($request->input())->update(['otp'=>"0"]);
+        return response()->json(['msg' =>'Succes', 'data'=>'Verified']);
+        }else{
+            return response()->json(['msg' =>'Faild', 'data' => 'unthurized']);
+        }
+        
     }
-    function setPassword(){
-    
+    function setPassword(Request $request){
+        $pass = $request->input('password');
+        User::where($request->input())->update(['password'=>$pass ]);
+        return response()->json(['msg' =>'Succes', 'data'=>'Updated']);
     }
     function profileUpdate(){
     
---
+++ Inventory/resources/views/email/OTP.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Your One-Time Password (OTP)</title>
+</head>
+<body>
+    <table width="100%" bgcolor="#f6f8f1" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>
+                <table align="center" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse; margin: auto; background-color: #ffffff; font-family: Arial, sans-serif; color: #333;">
+                    <tr>
+                        <td align="center" style="padding: 20px 0;">
+                            <img src="[Your Company Logo]" alt="Logo" width="150">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 20px;">
+                            <h2 style="color: #333; font-size: 24px; margin-bottom: 20px;">Your One-Time Password </h2>
+                            <p>Dear [Recipient's Name],</p>
+                            <p>Thank you for choosing our services. To ensure the security of your account, we have generated a One-Time Password (OTP) for you. Please find the details below:</p>
+                            <table cellspacing="0" cellpadding="10" width="100%" style="border-collapse: collapse; border: 1px solid #ccc;">
+                                <tr>
+                                    <td align="center" bgcolor="#f6f8f1" style="font-size: 18px; font-weight: bold; border-bottom: 1px solid #ccc;">OTP:</td>
+                                    <td align="center" bgcolor="#f6f8f1" style="font-size: 18px; border-bottom: 1px solid #ccc;">{{$otp}} </td>
+                                </tr>
+                            </table>
+                            <p>Please use the above OTP within [Insert Time Limit Here] to complete the verification process. If you did not request this OTP or have any concerns about your account security, please contact our support team immediately.</p>
+                            <p>Please note that this OTP is valid for a single use and should not be shared with anyone else. Our company will never ask for your OTP or any sensitive information.</p>
+                            <p>If you have any questions or require further assistance, feel free to reach out to our customer support team at [Insert Contact Details Here].</p>
+                            <p>Thank you for your cooperation.</p>
+                            <p>Best regards,</p>
+                            <p>[Your Company Name]</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
